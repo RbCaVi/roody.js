78475dacb897e66764d2bb559e67711af742ee8e
RbCaViTerminal <robert@robertvail.info>
2024-01-20 13:33:17 -0500

    add register.getCommandData

--- register.js
+++ register.js
@@ -7,3 +7,13 @@ export async function registerCommand(client,filePath) {
 		throw new Error(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
+export async function getCommandData(client,filePath) {
+	const command = await import(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		return command.data.toJSON();
+	} else {
+		throw new Error(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
