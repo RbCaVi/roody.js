7d3d71c13e6d1405bdf1bfc5d32562a52c8757d3
RbCaViTerminal <robert@robertvail.info>
2024-01-20 11:57:45 -0500

    convert to es module

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import {readFileSync} from 'node:fs'
-const token=readFileSync('token',{'encoding':'utf-8'});
+import * as discord from 'discord.js';
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+const token=fs.readFileSync('token',{'encoding':'utf-8'});
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
+client.once(discord.Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
