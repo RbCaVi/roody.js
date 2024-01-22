2a490f768f7b5fda283040cc4cb275318291958c
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2024-01-22 15:16:23 +0000

    add a test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+import { makeimage } from "./block.js";
+import * as fs from "fs";
+
+/*const blocks = [
+  ["iron_bar", { "type": "wire_spool", "rotate": 2, "weld": [false, false, true, false] }],
+  ["air", "iron_bar"],
+];*/
+const blocks = [["iron_bar"]];
+const im = await makeimage(blocks);
+const data = im.toBuffer("image/png");
+fs.writeFileSync("./image.png", data);
+//const out = fs.createWriteStream("output.png");
+//const stream = im.createPNGStream();
+//stream.pipe(out);
