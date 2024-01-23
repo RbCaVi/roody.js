f24e6c8681ab3a9c8dc15790b0420e18b6acb7ad
RbCaVi <92527341+RbCaVi@users.noreply.github.com>
2024-01-23 18:14:46 +0000

    New blocks array

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 import { makeimage } from "./block.js";
 import * as fs from "fs";
 
-/*const blocks = [
+let blocks;
+blocks = [
   ["iron_bar", { "type": "wire_spool", "rotate": 2, "weld": [false, false, true, false] }],
   ["air", "iron_bar"],
-];*/
-const blocks = [["iron_bar"]];
+];
+//blocks = [["iron_bar"]];
+blocks=[["iron_bar",{type:"wire_spool",rotate:1,weld:"all"},"iron_bar"]];
 const im = await makeimage(blocks);
 const data = im.toBuffer("image/png");
 fs.writeFileSync("./image.png", data);
